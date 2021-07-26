9fc9b77476fbf80541734b70c7ed5fd9d97e3db5
zhaojie_linux <1183533202@qq.com>
2021-07-26 16:31:54 +0100

    create the directory 'arch'

---
+++ arch/archSize.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+	printf("This will print the length of Bytes of Variable for Local System.\n");
+	printf("The length of \"int\": %d\n",sizeof(int));
+	printf("The length of \"long\": %d\n",sizeof(long));
+	printf("The length of \"longlong\": %d\n",sizeof(long long));
+	printf("The length of \"short\": %d\n",sizeof(short));
+	printf("The length of \"char\": %d\n",sizeof(char));
+	printf("The length of \"float\": %d\n",sizeof(float));
+	printf("The length of \"double\": %d\n",sizeof(double));
+	return 0;
+}
+
